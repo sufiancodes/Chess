089a820b9fe140d73ee54981eceb59e98b07d688
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-03-14 20:25:48 +0500

    feat: add display method to ui

--- lib/chess/ui.rb
+++ lib/chess/ui.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
-
+require_relative 'board'
 # this class deals with UI player input etc
 class UI
-  def initialize
+  def display(board)
+    @board = board
+    puts board
   end
 end
+board = Board.new
+mui = UI.new
+mui.display(board)
