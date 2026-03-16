9cbc9d3d345c3a6fe99498c43422013224cae784
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-03-16 20:44:43 +0500

    feat: add method to list legal moves of pawn at a any given condition

--- lib/chess/pieces/pawn.rb
+++ lib/chess/pieces/pawn.rb
@@ -1,23 +1,55 @@
 class Pawn
+  WHITE_PAWN = "\u265f".freeze
+  BLACK_PAWN = "\u2659".freeze
+
+  attr_reader :color
+
   def initialize(color, position = nil)
     @color = color
     @position = position
   end
 
   def to_s
-    @color == 'white' ? "\u265f" : "\u2659"
+    @color == 'white' ? WHITE_PAWN : BLACK_PAWN
   end
 
   def list_legal_moves(board, row, col)
-    # piece = board[row][col]
-    # board[row][col] = "\u2610"
-    # board[row + 1][col] = piece
-    # now just need to implement its rules
-    if row
+    # I still need to catch it at boundaries
+    if color == 'black' && row == 1 && !board[row + 1] == "\u2610"
+      "#{board[row + 1][col]} or #{board[row + 2][col]}"
+    elsif color == 'white' && row == 6 && !board[row - 1] == "\u2610"
+      "#{board[row - 1][col]} or #{board[row - 2][col]}"
+    elsif color == 'black' && !board[row + 1] == "\u2610"
+      'cant move'
+    elsif color == 'white' && !board[row - 1] == "\u2610"
+      'cant move'
+    elsif color == 'black' && (!board[row + 1][col + 1] == "\u2610" || !board[row + 1][col - 1] == "\u2610")
+      if !board[row + 1][col + 1] == "\u2610" && !board[row + 1][col - 1] == "\u2610"
+        "#{board[row + 1][col + 1]} or #{board[row + 1][col - 1]}"
+      elsif !board[row + 1][col + 1] == "\u2610"
+        "#{board[row + 1][col + 1]}"
+      elsif !board[row + 1][col - 1] == "\u2610"
+        "#{board[row + 1][col - 1]}"
+      end
+    elsif color == 'white' && (!board[row - 1][col - 1] == "\u2610" || !board[row - 1][col + 1] == "\u2610")
+      if !board[row - 1][col + 1] == "\u2610" && !board[row - 1][col - 1] == "\u2610"
+        "#{board[row - 1][col + 1]} or #{board[row - 1][col - 1]}"
+      elsif !board[row - 1][col + 1] == "\u2610"
+        "#{board[row - 1][col + 1]}"
+      elsif !board[row - 1][col - 1] == "\u2610"
+        "#{board[row - 1][col - 1]}"
+      end
+    elsif color == 'black' && board[row + 1] == "\u2610"
+      "#{board[row + 1]}"
+    elsif color == 'white' && board[row - 1] == "\u2610"
+      "#{board[row - 1]}"
+    end
+    # now just movement of one is need to be Implement but that movement is only possible when the spot is ☐ else no but yes it can capture diagonal
   end
 end
-pawn = Pawn.new('black', 'a1')
-puts pawn
+
+# pawn = Pawn.new('black', 'a1')
+# puts pawn
 
 # Forward Only: A pawn moves directly forward one square at a time to an empty square.
 # The First-Move Option: If a pawn has not yet moved, it has the option to move two squares forward in a single turn, provided both squares are unoccupied.
