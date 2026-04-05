68bd892a2e3c495c9ed15e5bc674baf359ddf5a8
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-04-05 21:36:00 +0500

    feat: add calculate_black_pawn_moves method to calculate moves for black pawn

--- lib/chess/move_calculator.rb
+++ lib/chess/move_calculator.rb
@@ -8,39 +8,48 @@ module MoveCalculator
 
     case piece
     when Pawn
-      calculate_pawn_moves(row, col, color, piece)
+      calculate_pawn_moves(row, col, color, piece, board)
       # when Rook
       #   calculate_rook_moves(position, board, color)
     end
   end
 
-  def self.calculate_pawn_moves(row, col, color, piece)
+  def self.calculate_pawn_moves(row, col, color, piece, board)
     if color == 'white'
-      calculate_white_pawn_move(row, col, piece)
+      calculate_white_pawn_moves(row, col, piece, board)
     elsif color == 'black'
-      calculate_pawn_moves(row, col, piece)
+      calculate_black_pawn_moves(row, col, piece, board)
     end
   end
+
+  private
+
+  def self.calculate_black_pawn_moves(row, col, piece, board)
+    moves = []
+    next_square = board.piece_at(row + 1, col)
+    second_next_square = board.piece_at(row + 2, col)
+    left_diagonal = board.piece_at(row + 1, col - 1)
+    right_diagonal = board.piece_at(row + 1, col + 1)
+    empty = "\u2610"
+
+    # starter move when board is clear and pawn hasn't move yet
+    moves << [row + 2, col] if piece.has_move == false && next_square == empty && second_next_square == empty
+
+    # starter move when pawn hasn't move and board is little clear not too clear
+    moves << [row + 1, col] if piece.has_move == false && next_square == empty && second_next_square != empty
+
+    # regular one square movement
+    moves << [row + 1, col] if next_square == empty && piece.has_move == true
+
+    # capture left diagonal
+    moves << [row + 1, col - 1] if left_diagonal == empty && piece.color != 'black'
+
+    # capture right diagonal
+    moves << [row + 1, col + 1] if right_diagonal == empty && piece.color != 'black'
+
+    p moves
+  end
 end
-  #  so here even in this method i can create cases weather its black or white one and then run appropriate case
-  #   moves = []
-  #   next_square = board.piece_at(row + 1, col)
-  #   second_next_square = board.piece_at(row + 2, col)
-  #   right_diagonal = board.piece_at(row + 1, col + 1)
-  #   left_diagonal = board.piece_at(row + 1, col - 1)
-  #   if color == 'black' && piece.has_move == false && next_square == "\u2610" && second_next_square == "\u2610"
-  #     moves << [[row + 2] [col]
-  #   end
-  #   if color == 'black' && piece.has_move == false && next_square == "\u2610" && second_next_square != "\u2610"
-  #     moves << [[row + 1], [col]]
-  #   end
-  #   if color == 'black' && next_square == "\u2610"
-  #     moves << [[row + 1], [col]]
-  #   end
-  #   if right_diagonal != "\u2610"
-  #     moves << [[row + 1], [col + 1]]
-  #   end
-  #   if left_diagonal != "\u2610"
-  #     moves << [[row + 1], [col - 1]]
-  #   end
-  #   p moves
+# do the singleton class
+
+# why aren't you capturing bro
