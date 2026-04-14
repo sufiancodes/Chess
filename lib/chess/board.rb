e6b82b9cc7ee4537cfd12bbee539569bb54fc33b
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-04-14 20:03:32 +0500

    feat: add move_piece method to mutate board

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+require_relative 'move_calculator'
 require_relative 'pieces/pawn'
 # All logic related to board
 class Board
-  attr_accessor :board
+  include MoveCalculator
 
   EMPTY_SPOT = "\u2610"
   def initialize
@@ -14,35 +15,34 @@ class Board
   def to_s
     col_alphabets = "\n a b c d e f g h"
     row_numbers = [8, 7, 6, 5, 4, 3, 2, 1]
-    board.map.with_index { |row, index| "#{row_numbers[index]} " + row.join(' ').to_s }.join("\n") + col_alphabets
+    @board.map.with_index { |row, index| "#{row_numbers[index]} " + row.join(' ').to_s }.join("\n") + col_alphabets
   end
 
-  def at(row, col)
-    board[row][col]
+  def piece_at(row, col)
+    @board [row][col]
   end
 
   def possible_moves_from(row, col)
-    piece = at(row, col)
-    piece.move(board, row, col)
+    MoveCalculator.legal_moves(row, col, self)
   end
 
   def move_piece(from_row, form_col, to_row, to_col)
-    # this method will mutate board
+    piece = @board [from_row] [form_col]
+    @board [to_row] [to_col] = piece
+    @board [from_row] [form_col] = EMPTY_SPOT
   end
 
   private
 
   def populate_board
     @board [0] = ["\u2656", "\u2658", "\u2657", "\u2654", "\u2655", "\u2657", "\u2658", "\u2656"]
-    @board [1] = [Pawn.new('black'), "\u2659", "\u2659", "\u2659", "\u2659", "\u2659", "\u2659", "\u2659"]
-    @board [6] = ["\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F"]
+    @board [1] = [Pawn.new('black', 1, 0, false), Pawn.new('black', 1, 1, false), Pawn.new('black', 1, 2, false), Pawn.new('black', 1, 3, false), Pawn.new('black', 1, 4, false), Pawn.new('black', 1, 5, false), Pawn.new('black', 1, 6, false), Pawn.new('black', 1, 7, false)]
+    @board [6] = [Pawn.new('white', 6, 0, false), Pawn.new('white', 6, 1, false), Pawn.new('white', 6, 2, false), Pawn.new('white', 6, 3, false), Pawn.new('white', 6, 4, false), Pawn.new('white', 6, 5, false), Pawn.new('white', 6, 6, false), Pawn.new('white', 6, 7, false)]
     @board [7] = ["\u265C", "\u265E", "\u265D", "\u265A", "\u265B", "\u265D", "\u265E", "\u265C"]
   end
 end
 board = Board.new
 puts board
-# pawn = board.at(1, 0)
-# p pawn
-board.possible_moves_from(1, 0)
+# p board.possible_moves_from(6, 0)
+# board.move_piece(1, 0, 3, 0)
 puts board
-
