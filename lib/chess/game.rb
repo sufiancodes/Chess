3db334914f1eb7885aa3df7241cbafdca3d627a9
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-04-21 20:32:33 +0500

    feat: add method to translate array value into readable chess positions for user

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -9,7 +9,21 @@ class Game
     create_useable_array(position, col, row)
   end
 
+  def translate_machine_input(array)
+    col = { 0 => 'a', 1 => 'b', 2 => 'c', 3 => 'd', 4 => 'e', 5 => 'f', 6 => 'g', 7 => 'h' }
+    row = { '8' => 0, '7' => 1, '6' => 2, '5' => 3, '4' => 4, '3' => 5, '2' => 6, '1' => 7 }
+    create_readable_data(array, col, row)
+  end
+
   private
+
+  def create_readable_data(array, col, row)
+    row = array.first
+    column = array.last
+    suffix = row[row]
+    prefix = col[column]
+    prefix + suffix.to_s
+  end
 
   def create_useable_array(position, col_alphabets, row_numbers)
     pos = position.split('')
@@ -23,3 +37,8 @@ class Game
     result
   end
 end
+game = Game.new
+p game.translate_user_input('a1')
+p game.translate_machine_input([7, 0])
+# board = Board.new
+# puts board
