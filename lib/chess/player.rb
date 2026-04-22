0afe7c84956715616aeefd6a342fcc518307986f
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-04-22 20:22:52 +0500

    feat: add method to retrieve names of players

--- lib/chess/player.rb
+++ lib/chess/player.rb
@@ -1,10 +1,14 @@
 class Player
-
-  attr_accessor :first_player, :second_player
-
+  
   def initialize
     @first_player = first_player
     @second_player = second_player
   end
-  
+
+  def welcome
+    puts 'Enter the first player name'
+    @first_player = gets.chomp
+    puts 'Enter the second player name'
+    @second_player = gets.chomp
+  end
 end
