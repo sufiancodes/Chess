b399df0d7253d67d1e679938efb412d373c6f56d
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-04-24 20:08:43 +0500

    fix: create readable data method now returns correct output

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -3,26 +3,42 @@
 require_relative 'board'
 # This class will have logic related to game
 class Game
+  def initialize
+    @board = Board.new
+  end
+
   def translate_user_input(position)
     col = { 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7 }
     row = { '0' => 8, '1' => 7, '2' => 6, '3' => 5, '4' => 4, '5' => 3, '6' => 2, '7' => 1 }
     create_useable_array(position, col, row)
   end
 
-  def translate_machine_input(array)
+  def translate_computer_input(array)
     col = { 0 => 'a', 1 => 'b', 2 => 'c', 3 => 'd', 4 => 'e', 5 => 'f', 6 => 'g', 7 => 'h' }
     row = { '8' => 0, '7' => 1, '6' => 2, '5' => 3, '4' => 4, '3' => 5, '2' => 6, '1' => 7 }
     create_readable_data(array, col, row)
   end
 
+  def play
+    loop do
+      puts @board
+      puts 'Please chose the piece you wish to move'
+      input = gets.chomp
+      array = translate_user_input(input)
+      # prompt user to please select the given move
+      moves = @board.possible_moves_from(array)
+      p translate_computer_input([4, 0])
+    end
+  end
+
   private
 
   def create_readable_data(array, col, row)
-    row = array.first
-    column = array.last
-    suffix = row[row]
-    prefix = col[column]
-    prefix + suffix.to_s
+    row_number = array.first
+    col_number = array.last
+    suffix = row[row_number]
+    prefix = col[col_number]
+    prefix + suffix
   end
 
   def create_useable_array(position, col_alphabets, row_numbers)
@@ -38,7 +54,4 @@ class Game
   end
 end
 game = Game.new
-p game.translate_user_input('a1')
-p game.translate_machine_input([7, 0])
-# board = Board.new
-# puts board
+# game.play
