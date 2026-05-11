ea8cd513ed35d901d817d01f8f820702f2b67abb
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-05-11 20:34:30 +0500

    feat: add method to get an enemy color

--- lib/chess/pieces/knight.rb
+++ lib/chess/pieces/knight.rb
@@ -2,6 +2,8 @@
 
 # this class will have all data related to knight
 class Knight
+  attr_reader :color
+
   BLACK_KNIGHT = "\u2658"
   WHITE_KNIGHT = "\u265E"
   def initialize(color, row, col)
@@ -12,5 +14,9 @@ class Knight
 
   def to_s
     @color == 'white' ? WHITE_KNIGHT : BLACK_KNIGHT
+  end
+
+  def enemy_color
+    @color == 'white' ? BLACK_KNIGHT : WHITE_KNIGHT
   end
 end
