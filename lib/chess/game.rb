67896c4eba6c3b2771bd48f784a0102e4d1182ad
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-05-31 19:59:26 +0500

    feat: add empty_at? method to check if square is empty or not

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -15,7 +15,7 @@ class Game
   def translate_user_input(position)
     col = { 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7 }
     row = { '1' => 7, '2' => 6, '3' => 5, '4' => 4, '5' => 3, '6' => 2, '7' => 1, '8' => 0 }
-    p create_useable_array(position, col, row)
+    create_useable_array(position, col, row)
   end
 
   def translate_computer_input(array)
@@ -31,9 +31,7 @@ class Game
       puts @board
       puts "#{player.current_player}: Please chose the piece you wish to move"
       input = gets.chomp
-      p input
       array = translate_user_input(input)
-      p array
       moves = @board.possible_moves_from(array)
       if moves.empty?
         puts 'no possible move available'
