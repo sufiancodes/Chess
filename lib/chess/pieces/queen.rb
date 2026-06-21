3e02d2a050b23d8cf5fa192a5a0955a84f4754cf
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-06-21 20:16:50 +0500

    feat: add to_s method to queen class to display queen

--- lib/chess/pieces/queen.rb
+++ lib/chess/pieces/queen.rb
@@ -1,6 +1,6 @@
+# frozen_string_literal: true
 
 class Queen
-
   BLACK_QUEEN = "\u2655"
   WHITE_QUEEN = "\u265B"
   def initialize(color, row, col, has_moved)
@@ -8,5 +8,9 @@ class Queen
     @row = row
     @column = col
     @has_moved = false
+  end
+
+  def to_s
+    @color == "black" ? BLACK_QUEEN : WHITE_QUEEN
   end
 end
