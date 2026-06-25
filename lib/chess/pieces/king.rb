31944da6e3e75243bbcddf1a1392cb886a473df3
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-06-25 20:25:22 +0500

    feat: add to_s method to king class to render it on board

--- lib/chess/pieces/king.rb
+++ lib/chess/pieces/king.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class King
+  attr_reader :color, :row, :col
+
+  WHITE_KING = "\u265A"
+  BLACK_KING = "\u2654"
   def initialize(color, row, col, has_moved)
     @color = color
     @row = row
     @column = col
     @has_moved = false
+  end
+
+  def to_s
+    @color == "black" ? BLACK_KING : WHITE_KING
   end
 end
