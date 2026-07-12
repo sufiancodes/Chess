27270091d0afee5ac233c158b7769f2b4d4587ed
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-07-12 22:44:34 +0500

    fix: add instance variable for ui and remove all method for translating user input and computer input since they don't really belong to game class

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -2,6 +2,7 @@
 
 require_relative "board"
 require_relative "player"
+require_relative "ui"
 # This class will have logic related to game
 class Game
   attr_accessor :player
@@ -9,20 +10,8 @@ class Game
   def initialize
     @board = Board.new
     @player = Player.new
+    @ui = UI.new
     @current_name = nil
-  end
-
-  def translate_user_input(position)
-    col = { "a" => 0, "b" => 1, "c" => 2, "d" => 3, "e" => 4, "f" => 5, "g" => 6, "h" => 7 }
-    row = { "1" => 7, "2" => 6, "3" => 5, "4" => 4, "5" => 3, "6" => 2, "7" => 1, "8" => 0 }
-    create_useable_array(position, col, row)
-  end
-
-  def translate_computer_input(array)
-    col_map = ["a", "b", "c", "d", "e", "f", "g", "h"]
-    row_map = ["8", "7", "6", "5", "4", "3", "2", "1"]
-    row_index, col_index = array
-    (col_map[col_index] + row_map[row_index]).to_s
   end
 
   def play
@@ -62,15 +51,6 @@ class Game
 
       player.switch_player!
     end
-  end
-
-  private
-
-  def create_useable_array(position, col_alphabets, row_numbers)
-    pos = position.split("")
-    column = pos[0]
-    row = pos[1]
-    result = [row_numbers[row], col_alphabets[column]]
   end
 end
 
