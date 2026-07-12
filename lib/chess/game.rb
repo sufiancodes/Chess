b83b8f618e1b981c3930cfde0b9471e5444715a0
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-07-12 22:29:56 +0500

    fix: move was being printed vertically now it print moves horizontally with space now format is more readable

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'board'
-require_relative 'player'
+require_relative "board"
+require_relative "player"
 # This class will have logic related to game
 class Game
   attr_accessor :player
@@ -13,14 +13,14 @@ class Game
   end
 
   def translate_user_input(position)
-    col = { 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7 }
-    row = { '1' => 7, '2' => 6, '3' => 5, '4' => 4, '5' => 3, '6' => 2, '7' => 1, '8' => 0 }
+    col = { "a" => 0, "b" => 1, "c" => 2, "d" => 3, "e" => 4, "f" => 5, "g" => 6, "h" => 7 }
+    row = { "1" => 7, "2" => 6, "3" => 5, "4" => 4, "5" => 3, "6" => 2, "7" => 1, "8" => 0 }
     create_useable_array(position, col, row)
   end
 
   def translate_computer_input(array)
-    col_map = %w[a b c d e f g h]
-    row_map = %w[8 7 6 5 4 3 2 1]
+    col_map = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    row_map = ["8", "7", "6", "5", "4", "3", "2", "1"]
     row_index, col_index = array
     (col_map[col_index] + row_map[row_index]).to_s
   end
@@ -31,18 +31,35 @@ class Game
       puts @board
       puts "#{player.current_player}: Please chose the piece you wish to move"
       input = gets.chomp
+
+      # translating stage
       array = translate_user_input(input)
       moves = @board.possible_moves_from(array)
+
+      # re prompting when no move available
       if moves.empty?
-        puts 'no possible move available'
+        puts "no possible move available"
         redo
       end
-      puts 'Now select the move from below you wish to play'
-      moves.each { |element| puts translate_computer_input(element) }
+      puts "Now select the move from below you wish to play"
+
+      # showing user possible moves
+      moves.each { |element| print translate_computer_input(element) + " "  }
+
+      # processing user move
       ip = gets.chomp
-      iii = translate_user_input(ip)
-      @board.move_piece(array, iii)
+      destination = translate_user_input(ip)
+      @board.move_piece(array, destination)
+
+      # finding enemy king
+      piece = @board.piece_at(destination[0], destination[1])
+      enemy_king = @board.find_enemy_king(piece.enemy_color)
+
+      # showing board and switching player
       puts @board
+      # stop game if its checkmate
+      break if @board.check_mate?(enemy_king)
+
       player.switch_player!
     end
   end
@@ -50,7 +67,7 @@ class Game
   private
 
   def create_useable_array(position, col_alphabets, row_numbers)
-    pos = position.split('')
+    pos = position.split("")
     column = pos[0]
     row = pos[1]
     result = [row_numbers[row], col_alphabets[column]]
