d1e56514ac6f32581c0c4ab8786a89f59d99cf41
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-08-14 20:39:24 +0500

    feat: add method move_two_pieces to move to pieces at once for castling

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -12,7 +12,7 @@ require_relative "rule_engine"
 # All logic related to board
 class Board
   attr_reader :board
-  attr_accessor :ui
+  attr_accessor :ui, :rule_engine
 
   include MoveCalculator
 
@@ -22,6 +22,7 @@ class Board
     populate_board
     # @ui to be remove
     @ui = UI.new
+    @rule_engine = RuleEngine.new
   end
 
   def piece_at(row, col)
@@ -66,11 +67,25 @@ class Board
     true if piece.color == own_color
   end
 
+  # For castling I probably need a new method like move pieces which moves two pieces at once
+  def move_two_pieces(king_from, king_to, rook_from, rook_to)
+    king = @board[king_from[0]][king_from[1]]
+    rook = @board[rook_from[0]][rook_from[1]]
+    king.has_moved = true if king.instance_of?(King)
+    rook.has_moved = true if rook.instance_of?(Rook)
+    king.row = king_to[0]
+    king.col = king_to[1]
+    rook.row = rook_to[0]
+    rook.col = rook_to[1]
+    @board[king_to[0]][king_to[1]] = king
+    @board[king_from[0]][king_from[1]] = EMPTY_SPOT
+    @board[rook_to[0]][rook_to[1]] = rook
+    @board[rook_from[0]][rook_from[1]] = EMPTY_SPOT
+  end
+
   def move_piece(from, to)
     piece = @board[from[0]][from[1]]
     piece.has_moved = true if piece.instance_of?(Pawn)
-    piece.has_moved = true if piece.instance_of?(King)
-    piece.has_moved = true if piece.instance_of?(Rook)
     piece.row = to[0]
     piece.col = to[1]
     @board[to[0]][to[1]] = piece
@@ -98,8 +113,8 @@ class Board
         Rook.new("black", 0, 0, false),
         Knight.new("black", 0, 1),
         Bishop.new("black", 0, 2, false),
-        King.new("black", 0, 3, false),
-        Queen.new("black", 0, 4, false),
+        Queen.new("black", 0, 3, false),
+        King.new("black", 0, 4, false),
         Bishop.new("black", 0, 5, false),
         Knight.new("black", 0, 6),
         Rook.new("black", 0, 7, false),
@@ -129,15 +144,26 @@ class Board
     @board[7] =
       [
         Rook.new("white", 7, 0, false),
-        Knight.new("white", 7, 1),
-        Bishop.new("white", 7, 2, false),
-        King.new("white", 7, 3, false),
-        Queen.new("white", 7, 4, false),
+        # Knight.new("white", 7, 1)
+        EMPTY_SPOT,
+        # Bishop.new("white", 7, 2, false)
+        EMPTY_SPOT,
+        # Queen.new("white", 7, 3, false)
+        EMPTY_SPOT,
+        King.new("white", 7, 4, false),
         Bishop.new("white", 7, 5, false),
         Knight.new("white", 7, 6),
         Rook.new("white", 7, 7, false),
       ]
   end
 end
-
+board = Board.new
+board.display_board(board.board)
+# white_king = board.piece_at(7, 4)
+# p board.rule_engine.queen_side_castle_possible?(white_king, board)
+# board.display_board(board.board)
+# board.board[7][4]
+# board.board[7][0]
+board.move_two_pieces([7, 4], [7, 2], [7, 0], [7, 3])
+board.display_board(board.board)
 # now after I fixed this (checkmate) I need to implement ent passant, castling, pawn promotion, serialization and then write tests and the project done.
