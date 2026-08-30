d6ac9488641d614818f82b1a0c94350c118fcedc
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-08-30 20:25:09 +0500

    refactor: remove repetitive code from queen and rook

--- lib/chess/pieces/queen.rb
+++ lib/chess/pieces/queen.rb
@@ -1,23 +1,12 @@
 # frozen_string_literal: true
 
-class Queen
-  attr_reader :color
-  attr_accessor :has_moved, :row, :col
-
+require_relative "piece"
+# this class has all data related to queen
+class Queen < Piece
   BLACK_QUEEN = "\u2655"
   WHITE_QUEEN = "\u265B"
-  def initialize(color, row, col, has_moved)
-    @color = color
-    @row = row
-    @col = col
-    @has_moved = false
-  end
 
   def to_s
     @color == "black" ? BLACK_QUEEN : WHITE_QUEEN
-  end
-
-  def enemy_color
-    @color == "white" ? "black" : "white"
   end
 end
--- lib/chess/pieces/rook.rb
+++ lib/chess/pieces/rook.rb
@@ -1,24 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "piece"
 # this class contain data related to Rook
-class Rook
-  attr_reader :color
-  attr_accessor :has_moved, :row, :col
-
+class Rook < Piece
   BLACK_ROOK = "\u2656"
   WHITE_ROOK = "\u265C"
-  def initialize(color, row, col, has_moved)
-    @color = color
-    @row = row
-    @col = col
-    @has_moved = false
-  end
 
   def to_s
     @color == "white" ? WHITE_ROOK : BLACK_ROOK
-  end
-
-  def enemy_color
-    @color == "white" ? "black" : "white"
   end
 end
