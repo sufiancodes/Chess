0297e7286c7a3f52b05f0d864b91c6fb723fed1a
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-08-30 20:13:33 +0500

    refactor: remove repetitive code for the pawn and knight class

--- lib/chess/pieces/knight.rb
+++ lib/chess/pieces/knight.rb
@@ -1,25 +1,11 @@
 # frozen_string_literal: true
-
+require_relative "piece"
 # this class will have all data related to knight
 class Knight
-  attr_reader :color
-  attr_accessor :has_moved, :row, :col
-
-
   BLACK_KNIGHT = "\u2658"
   WHITE_KNIGHT = "\u265E"
-  def initialize(color, row, col, has_moved)
-    @row = row
-    @col = col
-    @color = color
-    @has_moved = has_moved
-  end
 
   def to_s
     @color == "white" ? WHITE_KNIGHT : BLACK_KNIGHT
-  end
-
-  def enemy_color
-    @color == "white" ? "black" : "white"
   end
 end
--- lib/chess/pieces/pawn.rb
+++ lib/chess/pieces/pawn.rb
@@ -1,25 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "piece"
 # this class have the data related to pawn
-class Pawn
+class Pawn < Piece
   WHITE_PAWN = "\u265f"
   BLACK_PAWN = "\u2659"
 
-  attr_reader :color
-  attr_accessor :has_moved, :row, :col
-
-  def initialize(color, row, col, has_moved)
-    @color = color
-    @row = row
-    @col = col
-    @has_moved = false
-  end
-
   def to_s
     @color == "white" ? WHITE_PAWN : BLACK_PAWN
-  end
-
-  def enemy_color
-    @color == "white" ? "black" : "white"
   end
 end
