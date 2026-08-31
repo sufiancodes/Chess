7b69d1ac2a8235d6055277abefdd64c219b8f7eb
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-08-31 19:51:21 +0500

    feat: add to_h method which convert all pieces and their data in to hash to make all pieces saveable in json

--- lib/chess/pieces/piece.rb
+++ lib/chess/pieces/piece.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "json"
 # This class has all the common data related to pieces
 class Piece
   attr_reader :color
@@ -14,5 +15,10 @@ class Piece
 
   def enemy_color
     @color == "white" ? "black" : "white"
+  end
+
+  def to_h
+    hash = { type: self.class.to_s, color: @color, row: @row, col: @col, has_moved: @has_moved }
+    JSON.dump(hash)
   end
 end
