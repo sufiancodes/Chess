59f8f38d3763986f4679e0c3a0c63cc46e12c146
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-09-14 20:21:38 +0500

    test: write test for the current player method when turn is not zero

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,10 +5,17 @@ require_relative "../lib/chess/player"
 describe Player do
   let(:player) { Player.new }
 
-  it "it returns first player when turn is zero" do
+  it "returns first player when turn is zero" do
     player.first_player = "Alex"
     player.second_player = "Bob"
     player.turn = 0
     expect(player.current_player).to(eq("Alex"))
+  end
+
+  it "returns second player when turn is not zero" do
+    player.first_player = "Alex"
+    player.second_player = "Bob"
+    player.turn = 1
+    expect(player.current_player).to(eq("Bob"))
   end
 end
